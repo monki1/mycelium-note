ab881431e8ef624b2e3d5de30e5d7cc61003ba55
monki1 <114178697+monki1@users.noreply.github.com>
2023-06-15 18:58:18 -0400

    updated resetDB.js and psql directory

--- bin/resetDB.js
+++ bin/resetDB.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const fs = require('fs');
-const chalk = require('chalk');
+const chalk = require('kleur');
 const { Client } = require('pg');
+// const chalk = require("chalk");
+// import chalk from 'chalk';
 
 // PG connection setup
 const connectionString = process.env.DATABASE_URL || `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
@@ -9,27 +11,44 @@ const client = new Client({
     connectionString: connectionString
 });
 
-// Loads the schema files from db/schema
-const runSchemaFiles = async () => {
-    console.log(chalk.cyan(`-> Loading Schema Files ...`));
-    const schemaFilenames = fs.readdirSync('./db/schema');
+// Loads the drop files from psql/drop directory
+const runDropFiles = async () => {
+    console.log(chalk.cyan(`-> Loading Drop Files ...`));
+    const dropFilenames = fs.readdirSync('./psql/drop');
 
-    for (const fn of schemaFilenames) {
-        const sql = fs.readFileSync(`./db/schema/${fn}`, 'utf8');
-        console.log(`\t-> Running ${chalk.green(fn)}`);
-        await client.query(sql);
+    for (const fn of dropFilenames) {
+        try {
+            const sql = fs.readFileSync(`./psql/drop/${fn}`, 'utf8');
+            console.log(`\t-> Running ${chalk.green(fn)}`);
+            await client.query(sql);
+        } catch (err) {
+            console.log(`\t-> Error running ${chalk.red(fn)} - ${err}`);
+        }
     }
 };
 
-const runSeedFiles = async () => {
-    console.log(chalk.cyan(`-> Loading Seeds ...`));
-    const seedFilenames = fs.readdirSync('./db/seeds');
+// Loads the create files from psql/create directory
+const runCreateFiles = async () => {
+    console.log(chalk.cyan(`-> Loading Create Files ...`));
+    const createFilenames = fs.readdirSync('./psql/create');
 
-    for (const fn of seedFilenames) {
-        const sql = fs.readFileSync(`./db/seeds/${fn}`, 'utf8');
-        console.log(`\t-> Running ${chalk.green(fn)}`);
-        await client.query(sql);
+    for (const fn of createFilenames) {
+        try {
+            const sql = fs.readFileSync(`./psql/create/${fn}`, 'utf8');
+            console.log(`\t-> Running ${chalk.green(fn)}`);
+            await client.query(sql);
+        } catch (err) {
+            console.log(`\t-> Error running ${chalk.red(fn)} - ${err}`);
+        }
     }
+};
+
+const runSeedFile = async () => {
+    console.log(chalk.cyan(`-> Loading Seeds ...`));
+    const seedFile = fs.readFileSync('./psql/seeds.sql', 'utf8');
+
+    console.log(`\t-> Running ${chalk.green('seeds.sql')}`);
+    await client.query(seedFile);
 };
 
 const runResetDB = async () => {
@@ -37,8 +56,9 @@ const runResetDB = async () => {
         await client.connect();
         console.log(`-> Connected to PostgreSQL on ${process.env.DB_HOST} as ${process.env.DB_USER}...`);
 
-        await runSchemaFiles();
-        await runSeedFiles();
+        await runDropFiles();
+        await runCreateFiles();
+        await runSeedFile();
 
         await client.end();
         console.log(chalk.green('-> Reset completed successfully.'));
---
+++ psql/drop/1_drop_db.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS mycelium_db;
+
