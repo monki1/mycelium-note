ac6237079cb04e31fa594fdd4cec3aae6a7cc505
monki1 <114178697+monki1@users.noreply.github.com>
2023-06-15 16:59:07 -0400

    ERD and drop tables are Now consistent with actual schema

--- psql/drop_tables.sql
+++ psql/drop_tables.sql
@@ -7,5 +7,8 @@ DROP TABLE IF EXISTS tag_types;
 -- Drop the "notes" table
 DROP TABLE IF EXISTS notes;
 
+-- Drop the "secrets" table
+DROP TABLE IF EXISTS secrets;
+
 -- Drop the "users" table
 DROP TABLE IF EXISTS users;
