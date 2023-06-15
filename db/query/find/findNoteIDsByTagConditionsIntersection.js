aaed87ac2ce906315ac4cc1e3ddc7303b26aae04
monki1 <114178697+monki1@users.noreply.github.com>
2023-06-15 16:40:05 -0400

    added runtime reduction TODO

--- db/query/find/findNoteIDsByTagConditionsIntersection.js
+++ db/query/find/findNoteIDsByTagConditionsIntersection.js
@@ -1,6 +1,23 @@
 const singleCondition = require('./findNoteIDsByTagConditionSingle');
 
 async function findNoteIDsByTagConditionsIntersection(tagConditions, userId) {
+
+    //TODO - change to using the tagCondition with the smallest COUNT of tags
+    // then use the noteIds to get the note objects
+    // like await Note.getObjectById(noteId)
+    //TODO then filter the note objects by the other tagConditions
+    // Note object look like this:
+    //   {
+    //     id: 205,
+    //     user_id: 1,
+    //     content: '1686852635223',
+    //     updated_at: 2023-06-15T18:10:35.243Z,
+    //     tags: [ { id: 284, value: '78555814604877', type: '64540980737343' }, { id: 285, value: '84993958961181', type: '10530417730983' } ]
+    //   }
+
+
+
+
     const noteIdsByCondition = await Promise.all(
         tagConditions.map(async (condition) => {
             const { type, value, operator } = condition;
