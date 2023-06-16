70188126c3c81de434b914e9cac346f9d3eb3354
monki1 <114178697+monki1@users.noreply.github.com>
2023-06-16 14:56:19 -0400

    added todos for resetDB

--- bin/resetDB.js
+++ bin/resetDB.js
@@ -2,64 +2,55 @@ require('dotenv').config();
 const fs = require('fs');
 const chalk = require('kleur');
 const { Client } = require('pg');
-// const chalk = require("chalk");
-// import chalk from 'chalk';
+
+const USER = process.env.DB_USER;
+const PASSWORD = process.env.DB_PASS;
+
+
+const psqlDirectory = './psql/';
+const dropTables = process.argv.includes('--drop');
+const createTables = process.argv.includes('--create-tables');
+const createUser = process.argv.includes('--create-user');
+//TODO ^ check if create user is needed, should ask for superuser and password
+// connection string should be ^
+// based on input { -U user -P password }
+// add file paths to filePaths array, loop through with runSQLFile
+
 
 // PG connection setup
-const connectionString = process.env.DATABASE_URL || `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+//
+const connectionString = process.env.DATABASE_URL || `postgresql://${USER}:${PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 const client = new Client({
     connectionString: connectionString
 });
-
-// Loads the drop files from psql/drop directory
-const runDropFiles = async () => {
-    console.log(chalk.cyan(`-> Loading Drop Files ...`));
-    const dropFilenames = fs.readdirSync('./psql/drop');
-
-    for (const fn of dropFilenames) {
+const runSQLFile = async (fn, directory) => {
         try {
-            const sql = fs.readFileSync(`./psql/drop/${fn}`, 'utf8');
+            const sql = fs.readFileSync(`${directory}/${fn}`, 'utf8');
             console.log(`\t-> Running ${chalk.green(fn)}`);
             await client.query(sql);
         } catch (err) {
             console.log(`\t-> Error running ${chalk.red(fn)} - ${err}`);
         }
+};
+// Function to run SQL files from a directory
+const runSQLFilesFromDirectory = async (directory) => {
+    console.log(chalk.cyan(`-> Loading ${directory} Files ...`));
+    const filenames = fs.readdirSync(directory);
+
+    for (const fn of filenames) {
+        await runSQLFile(fn, directory);
     }
 };
 
-// Loads the create files from psql/create directory
-const runCreateFiles = async () => {
-    console.log(chalk.cyan(`-> Loading Create Files ...`));
-    const createFilenames = fs.readdirSync('./psql/create');
-
-    for (const fn of createFilenames) {
-        try {
-            const sql = fs.readFileSync(`./psql/create/${fn}`, 'utf8');
-            console.log(`\t-> Running ${chalk.green(fn)}`);
-            await client.query(sql);
-        } catch (err) {
-            console.log(`\t-> Error running ${chalk.red(fn)} - ${err}`);
-        }
-    }
-};
-
-const runSeedFile = async () => {
-    console.log(chalk.cyan(`-> Loading Seeds ...`));
-    const seedFile = fs.readFileSync('./psql/seeds.sql', 'utf8');
-
-    console.log(`\t-> Running ${chalk.green('seeds.sql')}`);
-    await client.query(seedFile);
-};
-
+// Function to reset the database
 const runResetDB = async () => {
     try {
         await client.connect();
         console.log(`-> Connected to PostgreSQL on ${process.env.DB_HOST} as ${process.env.DB_USER}...`);
 
-        await runDropFiles();
-        await runCreateFiles();
-        await runSeedFile();
-
+        await runSQLFilesFromDirectory('./psql/drop');
+        await runSQLFilesFromDirectory('./psql/create');
+        await runSQLFilesFromDirectory('./psql/seeds');
         await client.end();
         console.log(chalk.green('-> Reset completed successfully.'));
         process.exit();
