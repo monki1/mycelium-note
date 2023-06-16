1026e2f6bd4c404915627c8d19a775cb6590844b
monki1 <114178697+monki1@users.noreply.github.com>
2023-06-16 15:21:35 -0400

    minor fix: capitalized ID in filename

--- db/query/find/index.js
+++ db/query/find/index.js
@@ -1,5 +1,5 @@
 const Note = require("../../models/Note");
-module.exports.idsByTagEq = require('./findNoteIdsByTagEquality');
+module.exports.idsByTagEq = require('./findNoteIDsByTagEquality');
 module.exports.idsByTagConditionSingle = require('./findNoteIDsByTagConditionSingle');
 module.exports.idsByTagConditionsIntersection = require('./findNoteIDsByTagConditionsIntersection');
 module.exports.idsByTagConditionsUnion = require('./findNoteIDsByTagConditionsUnion');
@@ -7,7 +7,7 @@ const allNoteIDs = require('./findAllNoteIDs');
 // ____
 
 
-
+// ENTRY POINTS
 
 module.exports.byIntersection = async (tagConditions, userId) => {
     const ids = await module.exports.idsByTagConditionsIntersection(tagConditions, userId);
@@ -23,4 +23,6 @@ module.exports.all = async (userId) => {
     const ids = await allNoteIDs(userId);
     return await Promise.all(ids.map(async (id) => await Note.getObjectById(id)));
 }
+
+
 
