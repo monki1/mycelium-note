82f6594a6f9feb7a19db4b94a7664cb7df263516
monki1 <114178697+monki1@users.noreply.github.com>
2023-06-16 15:22:42 -0400

    empty file: search content contains

---
+++ db/query/find/findAllNoteIDsByContentContains.js
@@ -0,0 +1,13 @@
+/*
+const searchValue = 'PSG';
+Site.query((qb) => {
+  qb.whereRaw("data->>'team' = ?", searchValue);
+})
+.fetchAll()
+.then((results) => {
+  console.log(results.toJSON());
+})
+.catch((error) => {
+  console.error(error);
+});
+ */
